f979bdcccc898931c4a96397bf8326ade3a2e4b9
Nuh Yamin <nuhyamin31@gmail.com>
2025-10-08 15:25:23 +0700

    Add new release download link

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // Download functionality
 function downloadApp() {
     // APK hosted on Google Drive
-    const apkUrl = 'https://drive.google.com/file/d/1X6kRsLR_UAincD1cjj7d22_ATiWPqo5z/view?usp=drive_link';
+    const apkUrl = 'https://drive.google.com/file/d/1X6kRsLR_UAincD1cjj7d22_ATiWPqo5z/view?usp=sharing';
     
     // Create a temporary link element to trigger download
     const link = document.createElement('a');
